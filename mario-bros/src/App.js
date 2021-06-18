618e26b7ee5766dc7910d986d7eee660e86f188f
Cristina <cristoshiwassaki@gmail.com>
2021-06-17 23:07:52 -0300

    inicio de projeto

---
+++ mario-bros/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.png'
+import './app.scss';
+
+function App() {
+  return (
+    <>
+      <div className='logoIcon'>
+        <img className='logo-img' src={logo} alt='Logo MarioBros' />
+      </div>
+    </>
+  );
+}
+
+export default App;
